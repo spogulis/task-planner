3fd2b94b716f3144f25ee5820bb7e3ee2844d333
Renārs Grebežs <originalchc@gmail.com>
2018-04-24 16:54:08 +0300

    Added option list, defined new task and list of added tasks

--- planner.py
+++ planner.py
@@ -1,26 +1,38 @@
-print("Welcome to your new planning software")
+tasks = []
 
-task = {}
-master = []
+#Applications options
+def print_menu():
+    print("Welcome to your planning software")
+    print("[1] - List current tasks")
+    print("[2] - Add new task")
+    print("[3] - Delete all tasks")
+    print("[4] - Quit (and write to file)")
+
+#Add one task including title and status
+def add_new_task(tasks, task_name, task_status):
+    task = {}
+    task["Title"] = task_name
+    task["Status"] = task_status
+    tasks.append(task)
+
+#List all tasks on screen
+def list_all_tasks(tasks):
+    for task in tasks:
+        print("Title: %s" % task["Title"])
+        print("Status: %s" % task["Status"])
+
+#Program logic
 while True:
-    title = input("Please enter a task name: ")
-    status = input("Was the task done (yes/no)? ")
-    status.lower()
-    print("Your task was saved in the planner as: \"%s.\"" % title)
+    print_menu() #Show options menu
+    action = input("Choose an action: ...")
+    if action == "2":
+        task_name = input("Please enter a task name: ") #User creates task name/description
+        task_status = input("Was the task done (yes/no): ") #User enters task status
+        add_new_task(tasks, task_name, task_status.lower())
 
-    #Create a task dictionary
-    if status == "yes":
-        task[status] = "Done"
-    else:
-        task[status] = "Not done"
-    new = input("Would you like to enter another task? (yes/no")
-
-    if new == "no":
+    if action == "1":
+        list_all_tasks(tasks)
+        break
+    if action == "4":
         break
 
-    #Merge all tasks into a single dictionary
-
-    master.append(task)
-
-for task in master:
-    print("Title: %s" % task[title])
